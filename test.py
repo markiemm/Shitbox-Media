9a2dbf8df8e455e860f18253db359b847b5f3ca2
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2021-12-05 10:08:31 +0530

    uhh

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 import shutil
 total, used, free = shutil.disk_usage("/run/media/hirusha/Local Drive 1TB/")
 
-total_disk_space = "Total: %d GiB" % ((total // (2**30)))
+total_disk_space = "Total: %d GiB" % ((total // (2**30))) 
 print(total_disk_space)
-
-
-# Not what i expected, the server is in TB and this gived GB, idk to convert from GB to TB lol
